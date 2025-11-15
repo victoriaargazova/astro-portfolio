28760fdd7b6d5cd0acc7e764af64e905dfff1e03
victoriaargazova <victoria.argazova@student.howest.be>
2025-11-15 13:19:43 +0100

    Fixed content collections with astro 5 requirements

---
+++ src/content.config.ts
@@ -0,0 +1,29 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        tags: z.array(z.string()),
+    }),
+});
+
+const projects = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/projects' }),
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        pubDate: z.date(),
+        description: z.string().optional(),
+        tags: z.array(z.string()),
+    }),
+});
+
+export const collections = { blog, projects };
+
+
+
