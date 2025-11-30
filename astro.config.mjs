0e428b13e2d7ff3fce75aad9ba15bcd92c4e544b
victoriaargazova <victoria.argazova@student.howest.be>
2025-11-30 19:58:26 +0100

    Set site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://victoriaargazova.github.io',
+    base: '/astro-portfolio/',
+    output: 'static',
+});
